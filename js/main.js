dd4c3e92152006c9031f71933118c92374907b81
Tumpa <bsse1307@iit.du.ac.bd>
2025-05-12 12:22:08 +0600

    Revert "all components added"
    This reverts commit afdb87359390b65be1ebbde3c00c58dc05c5c03a.

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import { App } from './app.js';
-import { UI } from './ui.js';
-
-const app = new App();
-
-UI.startBtn.addEventListener('click', () => app.start());
-UI.restartBtn.addEventListener('click', () => window.location.reload());
